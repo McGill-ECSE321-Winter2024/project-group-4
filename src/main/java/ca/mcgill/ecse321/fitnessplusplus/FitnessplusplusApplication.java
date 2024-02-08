9edd2381cee3cab646d424e5b92ec72730cfeb91
Mathieu <matpestel@gmail.com>
2024-02-08 17:26:47 -0500

    Initial setup of folder structure

---
+++ src/main/java/ca/mcgill/ecse321/fitnessplusplus/FitnessplusplusApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.fitnessplusplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessplusplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessplusplusApplication.class, args);
+	}
+
+}
