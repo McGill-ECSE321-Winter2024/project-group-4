218e975132211a83285810d27ce8e3a0d3e1c4f5
Isbat-ulIslam <isbat-ul.islam@mail.mcgill.ca>
2024-02-14 17:48:34 -0500

    Update application file

--- src/main/java/ca/mcgill/ecse321/fitnessplusplus/FitnessplusplusApplication.java
+++ src/main/java/ca/mcgill/ecse321/fitnessplusplus/FitnessplusplusApplication.java
@@ -2,12 +2,20 @@ package ca.mcgill.ecse321.fitnessplusplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class FitnessplusplusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessplusplusApplication.class, args);
 	}
+
+	@RequestMapping("/")
+	public String greeting() {
+		return "Hello world!";
+	
 
 }
