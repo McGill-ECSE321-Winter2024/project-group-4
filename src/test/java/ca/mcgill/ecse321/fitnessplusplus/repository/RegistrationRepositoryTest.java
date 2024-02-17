54c2e5b921c53e22825b4f3fbd377e003e363ddc
yonatanbensimon <yonatan.bensimon@mail.mcgill.ca>
2024-02-17 15:44:58 -0500

    Update RegistrationRepositoryTest.java
    Identified the file to be modified and added a comment on the actions to be dome

--- src/test/java/ca/mcgill/ecse321/fitnessplusplus/repository/RegistrationRepositoryTest.java
+++ src/test/java/ca/mcgill/ecse321/fitnessplusplus/repository/RegistrationRepositoryTest.java
@@ -1,5 +1,15 @@
 package ca.mcgill.ecse321.fitnessplusplus.repository;
 
+/*
+PersistanceLayer Testing Registration
+
+Read test cases exist for each class
+Write test cases exist for each class
+Test suite demonstrates that application can read and write - objects
+Test suite demonstrates that application can read and write - attributes
+Test suite demonstrates that application can read and write - references
+Database contents cleared / reverted after test method
+ */
 public class RegistrationRepositoryTest {
     
 }
