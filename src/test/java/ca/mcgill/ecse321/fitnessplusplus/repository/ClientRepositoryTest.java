d292faaf8c21fa4351890b8a12d59734d2e61eaf
Mathieu <matpestel@gmail.com>
2024-02-17 15:59:17 -0500

    Started ClientRepositoryTest

--- src/test/java/ca/mcgill/ecse321/fitnessplusplus/repository/ClientRepositoryTest.java
+++ src/test/java/ca/mcgill/ecse321/fitnessplusplus/repository/ClientRepositoryTest.java
@@ -1,5 +1,17 @@
 package ca.mcgill.ecse321.fitnessplusplus.repository;
 
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ClientRepositoryTest {
-    
+    @Autowired
+    private ClientRepository clientRepository;
+    @Autowired
+    private RegisteredUserRepository registeredUserRepository;
+
+    @AfterEach
+    public void clearDatabase() {
+        clientRepository.deleteAll();
+        registeredUserRepository.deleteAll();
+    }
 }
