f2d78d3ed93e00679d6c07c9757083ee66274d23
yonatanbensimon <yonatan.bensimon@mail.mcgill.ca>
2024-02-18 13:14:49 -0500

    Created the clearDatabase function and imported some important classes

--- src/test/java/ca/mcgill/ecse321/fitnessplusplus/repository/RegistrationRepositoryTest.java
+++ src/test/java/ca/mcgill/ecse321/fitnessplusplus/repository/RegistrationRepositoryTest.java
@@ -10,6 +10,18 @@ Test suite demonstrates that application can read and write - attributes
 Test suite demonstrates that application can read and write - references
 Database contents cleared / reverted after test method
  */
+
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ca.mcgill.ecse321.fitnessplusplus.model.Registration;
+
+@SpringBootTest
 public class RegistrationRepositoryTest {
-    
+    @Autowired private RegistrationRepository registrationRepositoryTest;
+
+    @AfterEach
+    public void clearDatabase() {
+        registrationRepositoryTest.deleteAll();
+    }
 }
