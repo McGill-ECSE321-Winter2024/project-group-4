a342414450ad8bce98a2054aa362102b776e2a85
Xander <xandergott03@gmail.com>
2024-02-18 16:17:34 -0500

    Added id to OfferedClass and created a test
    Added an id field and a getter to OfferedClass, and created a test for OfferedClass

--- src/test/java/ca/mcgill/ecse321/fitnessplusplus/repository/OfferedClassRepositoryTest.java
+++ src/test/java/ca/mcgill/ecse321/fitnessplusplus/repository/OfferedClassRepositoryTest.java
@@ -1,5 +1,32 @@
 package ca.mcgill.ecse321.fitnessplusplus.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import ca.mcgill.ecse321.fitnessplusplus.model.OfferedClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class OfferedClassRepositoryTest {
-    
+    @Autowired private OfferedClassRepository offeredClassRepository;
+
+    @AfterEach
+    public void clearDatabase() {
+        offeredClassRepository.deleteAll();
+    }
+
+    @Test
+    public void testPersistAndLoadOfferedClass1() {
+        OfferedClass offeredClass = new OfferedClass();
+        offeredClassRepository.save(offeredClass);
+        int classId = offeredClass.();
+
+        offeredClass = offeredClassRepository.findOfferedClassByclassType(classType);
+
+        assertNotNull(offeredClass);
+        assertEquals(classType, offeredClass.getClassType());
+    }
 }
