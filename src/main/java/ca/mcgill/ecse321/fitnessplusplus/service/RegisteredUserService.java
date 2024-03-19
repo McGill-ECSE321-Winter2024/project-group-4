793874871d1080e49c5dbd3c304515d8f3c5458e
Mathieu <matpestel@gmail.com>
2024-03-19 17:30:56 -0400

    Implemented promoteUser

--- src/main/java/ca/mcgill/ecse321/fitnessplusplus/service/RegisteredUserService.java
+++ src/main/java/ca/mcgill/ecse321/fitnessplusplus/service/RegisteredUserService.java
@@ -1,12 +1,12 @@
 package ca.mcgill.ecse321.fitnessplusplus.service;
 
-import ca.mcgill.ecse321.fitnessplusplus.model.AccountRole;
-import ca.mcgill.ecse321.fitnessplusplus.model.Client;
-import ca.mcgill.ecse321.fitnessplusplus.model.RegisteredUser;
+import ca.mcgill.ecse321.fitnessplusplus.model.*;
 import ca.mcgill.ecse321.fitnessplusplus.repository.RegisteredUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class RegisteredUserService {
 
     @Autowired
@@ -21,6 +21,25 @@ public class RegisteredUserService {
         newUser.setAccountRole(newRole);
         registeredUserRepository.save(newUser);
         return newUser;
+    }
+
+    @Transactional
+    public RegisteredUser promoteUser(RegisteredUser registeredUser) throws Exception {
+        if (registeredUser.getAccountRole().getClass() == Client.class) {
+            registeredUser.setAccountRole(new Instructor());
+        }
+        else if (registeredUser.getAccountRole().getClass() == Instructor.class) {
+            registeredUser.setAccountRole(new Owner());
+        }
+        else if (registeredUser.getAccountRole().getClass() == Owner.class) {
+            throw new Exception("Cannot promote owner");
+        }
+        else {
+            // if for some reason the account doesn't have a role
+            registeredUser.setAccountRole(new Client());
+        }
+
+        return registeredUser;
     }
 
 }
