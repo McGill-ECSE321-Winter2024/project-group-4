94735ff8bde1f769b62551162769607afb458f42
Xander <xandergott03@gmail.com>
2024-03-22 13:06:24 -0400

    Update testOfferedClassService.java

--- src/test/java/ca/mcgill/ecse321/fitnessplusplus/service/testOfferedClassService.java
+++ src/test/java/ca/mcgill/ecse321/fitnessplusplus/service/testOfferedClassService.java
@@ -1,4 +1,44 @@
 package ca.mcgill.ecse321.fitnessplusplus.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import ca.mcgill.ecse321.fitnessplusplus.repository.InstructorRepository;
+import ca.mcgill.ecse321.fitnessplusplus.repository.OfferedClassRepository;
+import ca.mcgill.ecse321.fitnessplusplus.repository.ScheduledClassRepository;
+import ca.mcgill.ecse321.fitnessplusplus.model.Instructor;
+import ca.mcgill.ecse321.fitnessplusplus.model.OfferedClass;
+import ca.mcgill.ecse321.fitnessplusplus.model.ScheduledClass;
+
+@ExtendWith(MockitoExtension.class)
 public class testOfferedClassService {
+    @Mock
+    private OfferedClassRepository offeredClassRepository;
+    @InjectMocks
+    private OfferedClassService offeredClassService;
+
+    @BeforeEach
+    public void setMockOutput() {
+
+    }
 }
