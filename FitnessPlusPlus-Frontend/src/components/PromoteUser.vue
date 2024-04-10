14960aa056c63750836587c05377164dd92f84fc
yonatanbensimon <yonatan.bensimon@mail.mcgill.ca>
2024-04-09 20:19:37 -0400

    Update PromoteUser.vue

--- FitnessPlusPlus-Frontend/src/components/PromoteUser.vue
+++ FitnessPlusPlus-Frontend/src/components/PromoteUser.vue
@@ -1,8 +1,46 @@
 <template>
-  <header id="header">
-      <h1>Promote User</h1>
-      <div class="logo">
-
-      </div>
-  </header>
+ <div id="content">
+   <header id="header">
+     <h1>Promote User</h1>
+     <div id="logo">
+       <img src="../assets/logo.png"/>
+     </div>
+   </header>
+ </div>
 </template>
+
+<style>
+
+#header {
+  display: flex;
+  align-items: center;
+  position: relative;
+  padding: 1em;
+}
+
+#header::before {
+  content: '';
+  display: block;
+
+  width: auto;
+  height: 8rem;
+}
+
+#header h1 {
+  margin: 0 auto;
+  font-size: 3vw;
+  line-height: 1;
+}
+
+#logo {
+  position: absolute;
+  right: 1em;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+  #logo img {
+    height: 8vw;
+    width: auto;
+  }
+</style>
